6de1ffb645ff4d207f6343fbfc6c362705a4ba48
Sergey <redmoloch@yandex.ru>
2021-11-04 23:35:36 +0300

    deleted commented code

--- project/src/utils/mocks.ts
+++ project/src/utils/mocks.ts
@@ -3,39 +3,6 @@ import { CITIES } from '../constants';
 import { adaptCommentFromServer, adaptHotelFromServer } from '../services/adapters';
 import { Comment, Offer, ServerComment, ServerOffer } from '../types/types';
 
-// export const makeFakeOffer = (favorite = false): Offer => ({
-//   price: +commerce.price(),
-//   rating: datatype.float({min: 1, max: 5}),
-//   title: name.title(),
-//   type: 'Apartment',
-//   previewImage: image.image(),
-//   city: {
-//     location: {
-//       latitude: parseFloat(address.latitude()),
-//       longitude: parseFloat(address.longitude()),
-//       zoom: 10},
-//     name: CITIES[datatype.number(5)],
-//   },
-//   location: {
-//     latitude: parseFloat(address.latitude()),
-//     longitude: parseFloat(address.longitude()),
-//     zoom: 10,
-//   },
-//   isPremium: datatype.boolean(),
-//   isFavorite: favorite ? true : datatype.boolean(),
-//   id: datatype.number(),
-//   maxAdults: datatype.number(6),
-//   bedrooms: datatype.number(6),
-//   description: commerce.productDescription(),
-//   goods: new Array(5).fill(null).map(() => commerce.productName()),
-//   images: new Array(5).fill(null).map(() => image.image()),
-//   host: {
-//     avatarUrl: image.image(),
-//     id: datatype.number(),
-//     isPro: datatype.boolean(),
-//     name: internet.userName(),
-//   },
-// });
 
 export const makeFakeServerOffer = (favorite = false): ServerOffer => ({
   price: +commerce.price(),
@@ -78,19 +45,6 @@ export const makeFakeOffer = (favorite = false): Offer => adaptHotelFromServer(m
 export const makeFakeOfferList = (): Offer[] => new Array(5).fill(null).map(() => makeFakeOffer());
 export const makeFakeFavoritesList = (): Offer[] => new Array(5).fill(null).map(() => makeFakeOffer(true));
 
-
-// export const makeFakeComment = (): Comment => ({
-//   comment: commerce.productDescription(),
-//   date: date.past().toDateString(),
-//   id: datatype.number(),
-//   rating: datatype.number(5),
-//   user: {
-//     avatarUrl: internet.avatar(),
-//     id: datatype.number(),
-//     isPro: datatype.boolean(),
-//     name: internet.userName(),
-//   },
-// });
 
 export const makeFakeServerComment = (): ServerComment => ({
   comment: commerce.productDescription(),
